f6f56664e89eac680b095f917458c2aab479badd
Nikolay Boldar <nboldar1@gmail.com>
2019-06-10 20:48:07 +0300

    not found fix with reload page

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,9 +3,15 @@ import Router from 'koa-router'
 
 const router = new Router()
 const usersController = new UsersController()
+
 /**
  * здесь мы прописывает роуты запросов
  */
+
+router.redirect('/users', '/')
+router.redirect('/login', '/')
+router.redirect('/signup', '/')
+
 router.get('/api/v1/users/', async (ctx, next) => {
   await usersController.index(ctx)
 })
